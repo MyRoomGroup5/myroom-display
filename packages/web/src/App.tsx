148d40a58c297d2036c1573fd89290191dabd349
anhn4n <1055279234@qq.com>
2022-05-31 01:04:32 +0800

    fix: 解决部分RoomPage冲突

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,35 +1,15 @@
 import './App.css'
-<<<<<<< HEAD
-import PriceComponent from './components/PriceComponent'
-import CarouselComponent from './components/CarouselComponent'
-import ApartmentComponent from './components/ApartmentComponnet'
-import ApartmentDetailComponent from './components/ApartmentDetailComponent'
-import CommentCardComponent from './components/CommentCardComponent'
-import CommentPageCompnent from './components/CommentPageComponent'
+
 import RoomDetailComponent from './components/RoomDetailCompoment'
 import RoomCard from './components/RoomCard'
-=======
 import RoomDetailPage from '@/pages/RoomDetailPage'
-import Login from '@/pages/Login'
->>>>>>> b507634c0304cd8872a3fc7382512f0e4b295881
 
 function App() {
   return (
     <div className="flex-column app">
-<<<<<<< HEAD
-      {/* <CarouselComponent></CarouselComponent>
-      <PriceComponent></PriceComponent>
-      <CommentCardComponent></CommentCardComponent> */}
-      {/* <ApartmentComponent></ApartmentComponent> */}
-      {/* <ApartmentDetailComponent></ApartmentDetailComponent> */}
-      {/* <ApartmentDetailComponent /> */}
-      {/* <CommentPageCompnent></CommentPageCompnent> */}
       <RoomDetailComponent></RoomDetailComponent>
-      {/* <RoomCard></RoomCard> */}
-=======
-      {/*<Login/>*/}
+      <RoomCard></RoomCard>
       <RoomDetailPage />
->>>>>>> b507634c0304cd8872a3fc7382512f0e4b295881
     </div>
   )
 }
