628bc6485440ff5f6004cbd08a84ff626befc716
朱航 <zhuhang@stu.xmu.edu.cn>
2022-06-04 22:31:35 +0800

    fix: 合并Mock数据，测试数据请求

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,27 @@
+import axios from 'axios'
+import { useEffect } from 'react'
 import './App.css'
 import Login from './pages/Login'
 import RoomDetailComponent from './components/RoomDetailCompoment'
 import RoomCard from './components/RoomCard'
 import RoomDetailPage from '@/pages/RoomDetailPage'
 import RoomActivityPage from './pages/RoomActivityPage'
+import roomDetailData from './mock/roomDetailData.json'
+import activityData from './mock/activityData.json'
+
 function App() {
+  useEffect(() => {
+    console.log('开始请求数据')
+    axios
+      .get('https://easydoc.net/mock/rJSvw8AF/p/38281680/B8YstYrl')
+      .then((res) => {
+        console.log('res: ', res)
+      })
+      .catch((err) => {
+        console.log('err: ', err)
+      })
+  })
+
   return (
     <div className="flex-column app">
       {/* <RoomDetailComponent />
